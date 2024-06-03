7157f057de90024fdce1c3ba1fe02668e04c850d
LiangbingQiangweiKaishaHexi1314 <yj2004vantor@gmail.com>
2024-06-02 22:05:13 -0400

    add multiply function and read write from and to file methods to polynomial and rewrite the test casese in Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,55 @@
+import java.io.File;
+import java.util.Arrays;;
+
 public class Driver {
-	public static void main(String [] args) {
-	Polynomial p = new Polynomial();
-	System.out.println(p.evaluate(3));
-	double [] c1 = {6,0,0,5};
-	Polynomial p1 = new Polynomial(c1);
-	double [] c2 = {0,-2,0,0,-9};
-	Polynomial p2 = new Polynomial(c2);
-	Polynomial s = p1.add(p2);
-	System.out.println("s(0.1) = " + s.evaluate(0.1));
-	if(s.hasRoot(1))
-	System.out.println("1 is a root of s");
-	else
-	System.out.println("1 is not a root of s");
-	}
-	}
+	public static void main(String [] args) 
+	{	
+		double [] c1 = {1,2,3};
+		int [] d1 = {0,1,2};
+		Polynomial p1 = new Polynomial(c1, d1);
+		/*
+		double [] c2 = {1,2,3};
+		int [] d2 = {1,2,3};
+		Polynomial p2 = new Polynomial(c2, d2);
+
+		double [] c3 = {1,6,2,3,5};
+		int [] d3 = {2,3,4,0,5};
+		Polynomial p3 = new Polynomial(c3, d3);
+
+		Polynomial result1 = p1.add(p2);
+		Polynomial result2 = p3.add(p1);
+
+		System.out.println(Arrays.toString(result2.coefficient));
+		System.out.println(Arrays.toString(result2.degree));*/
+
+		double [] c4 ={1,1};
+		int [] d4 ={1,0};
+		Polynomial p4 = new Polynomial(c4, d4);
+
+		double [] c5 ={1,1};
+		int [] d5 ={1,0};
+		Polynomial p5 = new Polynomial(c5, d5);
+
+		Polynomial result3 = p5.multiply(p1);
+		//System.out.println(Arrays.toString(result3.coefficient));
+		//System.out.println(Arrays.toString(result3.degree));
+
+		/*String polynomial = "5-3x2+7x8";
+		String regex = "(?<=op)|(?=op)".replace("op", "[+\\-x]");
+        String[] tokens = polynomial.split(regex);
+		System.out.println(Arrays.toString(tokens));*/
+
+		File file = new File("poly.txt");
+		Polynomial p6 = new Polynomial(file);
+		//System.out.println(Arrays.toString(p6.coefficient));
+		//System.out.println(Arrays.toString(p6.degree));
+
+		//p6.saveToFile("poly1.txt");
+		result3.saveToFile("poly1.txt");
+		
+		File file1 = new File("poly1.txt");
+		Polynomial p7 = new Polynomial(file1);
+		System.out.println(Arrays.toString(p7.coefficient));
+		System.out.println(Arrays.toString(p7.degree));
+	}	
+}
--- Polynomial.java
+++ Polynomial.java
@@ -1,44 +1,227 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Polynomial 
 {   
     // fields
-    double[] coefficient;
+    double coefficient[];
+    int degree[];
 
     // methods
     public Polynomial()
     {
         coefficient = new double[]{0.0};
+        degree = new int[]{0};
     }
 
-    public Polynomial(double[] coeffs)
-    {
-        coefficient = new double [coeffs.length];
+    public Polynomial(double[] coeffs, int degs[])
+    {   
+        coefficient = new double[coeffs.length];
+        degree = new int[degs.length];
+        //initialize coefficients and degrees:
+        this.coefficient = new double[coeffs.length];
         for (int i=0; i<coeffs.length; i++)
         {
-            coefficient[i] = coeffs[i];
+            this.coefficient[i] = coeffs[i];
+            this.degree[i] = degs[i];
+        }
+    }
+
+    // Constructor with a File argument
+    public Polynomial(File poly_file) 
+    {
+        try (BufferedReader br = new BufferedReader(new FileReader(poly_file))) 
+        {
+            String polyString = br.readLine();
+            initializeFromPolynomialString(polyString);
+        } catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to initialize from polynomial string
+    private void initializeFromPolynomialString(String polyString) 
+    {
+        List<Double> coeflist = new ArrayList<>();
+        List<Integer> deglist = new ArrayList<>();
+
+        String[] tokens = splitByOperators(polyString);
+
+        double curCoeff = 0.0;
+        int curDed = 0;
+        boolean nIsNeg = false;
+
+        for (int i = 0; i < tokens.length; i++) 
+        {
+            if (tokens[i].matches("[+-]?\\d*\\.?\\d+")) 
+            {
+                curCoeff = Double.parseDouble(tokens[i]);
+                if (nIsNeg) {
+                    curCoeff = -curCoeff;
+                    nIsNeg = false;
+                }
+                if (i + 1 < tokens.length && tokens[i + 1].equals("x")) {
+                    if (i + 2 < tokens.length && tokens[i + 2].matches("\\d+")) {
+                        curDed = Integer.parseInt(tokens[i + 2]);
+                        i += 2; // Skip "x" and the degree
+                    } else {
+                        curDed = 1;
+                        i++; // Skip "x"
+                    }
+                }
+                coeflist.add(curCoeff);
+                deglist.add(curDed);
+            } else if (tokens[i].equals("x")) {
+                curDed = 1;
+                coeflist.add(curCoeff);
+                deglist.add(curDed);
+            } else if (tokens[i].equals("+")) {
+                continue;
+            } else if (tokens[i].equals("-")) {
+                nIsNeg = true;
+            }
+        }
+
+        coefficient = new double[coeflist.size()];
+        degree = new int[deglist.size()];
+
+        for (int i = 0; i < coeflist.size(); i++) 
+        {
+            coefficient[i] = coeflist.get(i);
+            degree[i] = deglist.get(i);
+        }
+    }
+
+    // Method to split the expression into tokens
+    private String[] splitByOperators(String expression) 
+    {
+        // Regular expression to split by arithmetic operators: +, -, x
+        String regex = "(?<=op)|(?=op)".replace("op", "[+\\-x]");
+        return expression.split(regex);
+    }
+    
+    public void saveToFile(String filename) 
+    {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filename))) 
+        {
+            for (int i = 0; i < coefficient.length; i++) 
+            {
+                if (i > 0 && coefficient[i] >= 0) 
+                {
+                    writer.print("+");
+                }
+                writer.print(coefficient[i]);
+                if (degree[i] != 0) 
+                {
+                    writer.print("x");
+                    if (degree[i] != 1) 
+                    {
+                        writer.print(degree[i]);
+                    }
+                }
+            }
+            writer.println();
+        } catch (IOException e) 
+        {
+            e.printStackTrace();
         }
     }
 
     public Polynomial add(Polynomial other)
     {   
-        int maxlength = Math.max(this.coefficient.length, other.coefficient.length);
-        double[] new_poly_coef = new double[maxlength];
+        int f_length = this.degree.length;
 
-        for(int i=0; i<maxlength; i++)
-        {
-            double this_coef = i<this.coefficient.length? this.coefficient[i] : 0.0;
-            new_poly_coef[i] = this_coef + other.coefficient[i];
+        //Determine the length of the final array(find the total number of unique degrees in the two polys):
+        for(int i=0; i<other.degree.length; i++)
+        {   
+            boolean exist = false;
+            for(int j=0; j<this.degree.length; j++)
+            {
+                if(this.degree[j] == other.degree[i])
+                {
+                    exist = true;
+                    break;
+                }
+            }
+            if(!exist)
+            {
+                f_length += 1;
+            }
         }
 
-        return new Polynomial(new_poly_coef);
+        //initialize the final degree and coefficient arrays with "this poly":
+        int[] f_degree = new int[f_length];
+        double[] f_coefficient = new double[f_length];
+
+        for(int i=0; i<this.degree.length; i++)
+        {
+            f_degree[i] = this.degree[i];
+            f_coefficient[i] = this.coefficient[i];
+        }
+        
+        //'index' helps to track the position in the final array
+        int index = this.degree.length;
+
+        /*Nested loop to add the other poly to the final poly
+         * 1.same
+         * 2.not unique
+        */
+
+        for (int i=0; i<other.degree.length; i++)
+        {   
+            boolean exist = false;
+            for(int j=0; j<this.degree.length; j++)
+            {
+                if(other.degree[i] == this.degree[j])
+                {
+                    f_coefficient[j] += other.coefficient[i];
+                    exist = true;
+                    break;
+                }
+            }
+            if(!exist)
+            {
+                f_coefficient[index] = other.coefficient[i];
+                f_degree[index] = other.degree[i];
+                index += 1;
+            }
+        }
+
+        return new Polynomial(f_coefficient, f_degree);
+
+    }
+
+    public Polynomial multiply(Polynomial other)
+    {
+        Polynomial f_poly = new Polynomial();
+
+        for(int i=0; i<this.degree.length; i++)
+        {   
+            Polynomial i_poly = new Polynomial(other.coefficient, other.degree);
+            for(int j=0; j<other.degree.length; j++)
+            {
+                i_poly.coefficient[j] *= this.coefficient[i];
+                i_poly.degree[j] += this.degree[i];
+            }
+            f_poly = f_poly.add(i_poly);
+        }
+        return f_poly;
     }
 
     public double evaluate(double value)
     {
-        double accum = this.coefficient[0];
+        double accum = 0;
         
         for(int i=1; i<this.coefficient.length; i++)
         {
-            accum = accum + this.coefficient[i] * Math.pow(value, i);
+            accum = accum + this.coefficient[i] * Math.pow(value, this.degree[i]);
         }
 
         return accum;
